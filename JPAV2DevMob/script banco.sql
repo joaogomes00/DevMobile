49ed7f5bc568d0f7ccd94db058ec44e6055eded8
João Pedro <79864997+jpzin10@users.noreply.github.com>
2021-12-03 20:42:22 -0300

    Add files via upload
    script banco

---
+++ JPAV2DevMob/script banco.sql
@@ -0,0 +1,27 @@
+
+create table artilheiroslib (
+    nome varchar(30) not null,
+    time varchar(30) not null,
+    gols int(2)
+);
+
+drop table artilheiroslib;
+
+create table artilheirosbr (
+    nome varchar(30) not null,
+    time varchar(30) not null,
+    gols int(2)
+);
+
+
+create table campeoeslib(
+    nome_campeao varchar(30) not null,
+    titulos int(2)
+);
+
+create table campeoesbr(
+    nome_campeao varchar(30) not null,
+    titulos int(2)
+);
+
+
